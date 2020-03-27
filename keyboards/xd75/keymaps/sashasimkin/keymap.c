aa10949ea318716dfb0747dcdeca674f893c2920
Alex Simkin <alex.simkin@tradecore.com>
2020-03-27 21:09:52 +0200

    update XD75 keymap

--- keyboards/xd75/keymaps/sashasimkin/keymap.c
+++ keyboards/xd75/keymaps/sashasimkin/keymap.c
@@ -66,7 +66,7 @@ void keyboard_post_init_user(void) {
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-   /* Base layer
+   /* Base layer - NOT USED
     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
     * │Esc│ 1 │ 2 │ 3 │ 4 │ 5 │PSc|Ins| 6 │ 7 │ 8 │ 9 │ 0 │ - │ = │
     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
@@ -80,12 +80,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
     */
     // KC_LSFT  -  KC_RSFT
+  // [L_BASE] = LAYOUT_ortho_5x15( \  // NOT USED
+  //   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_PSCR, KC_INS,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F11,  KC_F12,   \
+  //   KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, _______,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_DEL,  \
+  //   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MINS, KC_EQL,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,   \
+  //   KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, KC_RSPC,  \
+  //   KC_LCTL, KC_APP,  KC_LGUI, MO_FN,    LALT_T(KC_LBRC), LALT_T(KC_SPC), KC_SPC,  KC_SPC, RALT_T(KC_SPC),  RALT_T(KC_RBRC), MO_FN,      KC_RGUI, KC_PGUP, KC_PGDN, KC_RCTL   \
+  // ),
+
+   /* Base layer
+    * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+    * │Esc│ 1 │ 2 │ 3 │ 4 │ 5 │PSc|Ins| 6 │ 7 │ 8 │ 9 │ 0 │ - │ = │
+    * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+    * │ ` │ Q │ W │ E │ R │ T | Y │ U │ I │ O │ P │ [ | ] │Bsp│Del│
+    * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+    * │Tab│ A │ S │ D │ F │ G | H │ J │ K │ L │ ; │ ' │Ins|Prc│Ent│
+    * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+    * │LSf│ Z │ X │ C │ V │ B | N │ M │ , │ . │ / │ \ │   |   |RSf│
+    * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+    * │LCt│Mnu│LGu│FnL│LAl│Spc│Spc│Spc│Spc│RAl│FnL│RGu│PgU│PgD│Rct│
+    * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+    */
   [L_BASE] = LAYOUT_ortho_5x15( \
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_PSCR, KC_INS,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F11,  KC_F12,   \
-    KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, _______,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_DEL,  \
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MINS, KC_EQL,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,   \
-    KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, KC_RSPC,  \
-    KC_LCTL, KC_APP,  KC_LGUI, MO_FN,    LALT_T(KC_LBRC), LALT_T(KC_SPC), KC_SPC,  KC_SPC, RALT_T(KC_SPC),  RALT_T(KC_RBRC), MO_FN,      KC_RGUI, KC_PGUP, KC_PGDN, KC_RCTL   \
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_F11,  KC_F12,   \
+    KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, KC_DEL,  \
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_INS, KC_PSCR, KC_QUOT, KC_ENT,   \
+    KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, _______, _______, KC_RSPC,  \
+    KC_LCTL, KC_APP,  KC_LGUI, MO_FN,    KC_LALT, KC_SPC, KC_SPC,  KC_SPC, KC_SPC,  KC_RALT, MO_FN,      KC_RGUI, KC_PGUP, KC_PGDN, KC_RCTL   \
   ),
    /* Rest of the keys
     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
